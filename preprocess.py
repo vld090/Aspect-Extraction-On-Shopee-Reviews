4114fa6abc295bd80501b0fc6d3bb52942a5b31d
fatima_dy <136289175+pateemaa@users.noreply.github.com>
2025-05-16 18:21:54 +0800

    impleemented preprocessing to jupyter notebook

--- preprocess.py
+++ preprocess.py
@@ -1,38 +1,24 @@
 import re
 from transformers import AutoTokenizer
 
-tokenizer = AutoTokenizer.from_pretrained("jcblaise/roberta-tagalog-base")
+tokenizer = AutoTokenizer.from_pretrained("xlm-roberta-base")
 
 def remove_repetitive_symbols(text):
     text = re.sub(r'[!?.]{2,}', '', text)
     text = re.sub(r'"{2,}', '', text)
     return text
 
-def is_gibberish (text):
-
-    return False
-
-def remove_gibberish (text):
-    text_list = text.split(" ")
-    processed_list = []
-
-    for i in text_list:
-        if is_gibberish(i) == False:
-            processed_list.append(i)
-    return ' '.join(processed_list)
-
-def is_noninformative_review (text):
-
-    return False
-
 def bpe_tokenization (text):
     return tokenizer.tokenize(text)
 
 def process_data (text):
-    text = remove_repetitive_symbols(text)
-    
-    if is_gibberish(text) or is_noninformative_review(text):
-        return None
-    
-    tokens = bpe_tokenization(text)
+    cleaned = remove_repetitive_symbols(text)
+    tokens = bpe_tokenization(cleaned)
     return tokens
+
+# Test
+# if __name__ == "__main__":
+#     sample = 'goo very light lang..... ganda naman pero mas maganda ako!!!! hahahahahabababababababababababahahahahahahahahaha'
+#     print("Original:", sample)
+#     print("Cleaned:", remove_repetitive_symbols(sample))
+#     print("Tokens:", process_data(sample))
